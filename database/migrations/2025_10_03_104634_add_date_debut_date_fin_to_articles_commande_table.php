3876da46187eb8fab30c5375fd4578cacb718bda
Amine Cherkaoui <cherkaoui_amine@outlook.com>
2025-10-03 16:27:30 +0100

    Location by date feature added

--- database/migrations/2025_10_03_104634_add_date_debut_date_fin_to_articles_commande_table.php
+++ database/migrations/2025_10_03_104634_add_date_debut_date_fin_to_articles_commande_table.php
@@ -8,7 +8,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('articles_commande', function (Blueprint $table) {
-            $table->dateTime('y')->nullable()->after('prix_total');
+            $table->dateTime('date_debut')->nullable()->after('prix_total');
             $table->dateTime('date_fin')->nullable()->after('date_debut');
         });
     }
